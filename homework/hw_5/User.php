519d665c92a473b2200fc9ee9a31e45cdc782be4
tderenko <tarasid23@gmail.com>
2022-07-24 20:27:30 +0300

    [homework] number 5

--- homework/hw_5/User.php
+++ homework/hw_5/User.php
@@ -1,7 +1,23 @@
 <?php
-
+namespace app\homework\hw_5;
 
 class User
 {
+    public function __construct(
+        protected int $id,
+        protected string $email,
+        protected string $password,
+    ){
+        if (strlen($this->password) > 8) {
+            throw new \Exception('Password is to long!!!');
+        }
+    }
 
+    public function getUserData(): array
+    {
+        return [
+            'id' => $this->id,
+            'email' => $this->email
+        ];
+    }
 }
--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+
+use app\homework\hw_5\User;
+
 require_once 'vendor/autoload.php';
 $db = new PDO('pgsql:host=psql;dbname=postgres', 'postgres', 'pass');
-dd($db);
+
+try {
+    $user = new User('asd', 'email@test.com', 'password');
+} catch (TypeError $e) {
+    d($e->getMessage());
+}
+
+try {
+    $user = new User(123, 'email@test.com', 'very long password');
+} catch (Exception $e) {
+    d($e->getMessage() . ' in ' . $e->getFile() . ' on line ' . $e->getLine());
+}
+
+$user = new User(2, 'test@email.com', 'password');
+dd($user->getUserData());
