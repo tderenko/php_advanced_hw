6cd55e9f8c87eea66b2d2537e0e3249c82a32bc1
tderenko <tarasid23@gmail.com>
2022-07-31 15:46:20 +0300

    add xdebug to homework project

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-function sendNotification(\app\homework\hw_6\contracts\SenderInterface $sender) {
-    $sender->send();
-}
-
-$emailNofitication = new \app\homework\hw_6\EmailNotification(
-    'test@gmail.com',
-    'TestMessage'
-);
-$emailNofitication->setMessage('Test Email message');
-
-$smsNotification = new \app\homework\hw_6\SmsNotification('380999999999');
-$smsNotification->setMessage('Test SMS message');
-
-sendNotification($emailNofitication);
-sendNotification($smsNotification);
-
-
-$test = new \app\homework\hw_6\Test();
-echo $test->getSum();
+phpinfo();
+die;
