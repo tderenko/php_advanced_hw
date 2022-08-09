d8a2a5dfb355acac50f23bcaea2ce05d22384e51
tderenko <tarasid23@gmail.com>
2022-08-09 20:53:12 +0300

    change database to mysql

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
 
-use classes\Currency;
-use classes\Money;
+require_once 'vendor/autoload.php';
 
-spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-    if (!file_exists($path)) {
-        throw new Exception("Class \"$class\" in \"$path\" not found!!!");
-    }
-    include_once $path;
-});
+$db = new PDO('mysql:dbname=php_advance;host=db', 'root', 'root');
 
-$currency = new Currency('UAH');
-$money = new Money(123, $currency);
-
-echo '<pre>', print_r($money, true), '</pre>';
+dd($db);
