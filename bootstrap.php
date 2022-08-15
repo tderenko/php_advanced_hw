b55f5d56d99a6ba20ccb3b9fa19f3e7acc5eac21
tderenko <tarasid23@gmail.com>
2022-08-15 21:44:49 +0300

    [homework 13] PDO

--- bootstrap.php
+++ bootstrap.php
@@ -1,1 +1,11 @@
 <?php
+try {
+    global $dbConnection;
+    $dbConnection = new PDO('mysql:dbname=php_advance;host=db', 'root', 'root', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Database connection is failed! {$e->getMessage()}");
+}
+
--- index.php
+++ index.php
@@ -1,7 +1,44 @@
 <?php
+/**
+ * @var $dbConnection PDO
+ */
+
 
 require_once 'vendor/autoload.php';
 
-$db = new PDO('mysql:dbname=php_advance;host=db', 'root', 'root');
+$user = new \classes\User();
 
-dd($db);
+if (isset($_GET['id'])) {
+    $query = $dbConnection->prepare('SELECT * FROM users WHERE id = ?');
+    $query->setFetchMode(PDO::FETCH_CLASS, $user::class);
+    $query->execute([$_GET['id']]);
+    $user = $query->fetch() ?: $user;
+    if (!$user->getId()) {
+        $error = "the user id \"{$_GET['id']}\" is incorrect!!!";
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+        switch ($_POST['action']) {
+            case 'create table':
+                $user->createTable();
+                break;
+            case 'remove users':
+                $user->removeUsers($_POST['delete']);
+                break;
+            case 'add user':
+                $user->addUser($_POST);
+                break;
+            case 'drop table':
+                $user->dropTable();
+                break;
+        };
+        header('location: /');
+        die;
+    } catch (Exception $e) {
+        $error = $e->getMessage();
+    }
+}
+
+require_once 'views/user.php';
