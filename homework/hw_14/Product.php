8c6b76c2776a7af2a82e192977511a2e4a1262bd
tderenko <tarasid23@gmail.com>
2022-08-20 11:48:43 +0300

    [homework-14] Single Responsibility

---
+++ homework/hw_14/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace homework\hw_14;
+
+class Product
+{
+    public function get($name) {}
+    public function set($name, $value) {}
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+    public function show() {}
+    public function print() {}
+}
