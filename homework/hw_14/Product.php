38bf5362b7b1b9bfd9af8f6fc47651368aba6118
tderenko <tarasid23@gmail.com>
2022-08-20 19:26:37 +0300

    create new class ProductHandler

--- homework/hw_14/Product.php
+++ homework/hw_14/Product.php
@@ -6,9 +6,4 @@ class Product
 {
     public function get($name) {}
     public function set($name, $value) {}
-    public function save() {}
-    public function update() {}
-    public function delete() {}
-    public function show() {}
-    public function print() {}
 }
