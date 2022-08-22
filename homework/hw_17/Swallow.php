4691510d8bc23e6f2169831fea0c910c75eed575
tderenko <tarasid23@gmail.com>
2022-08-22 09:13:49 +0300

    [homework-17] Interface segregation

---
+++ homework/hw_17/Swallow.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace homework\hw_17;
+
+
+class Swallow implements interfaces\BirdInterface, interfaces\FlyAvailableInterface
+{
+
+    public function eat()
+    {
+        // TODO: Implement eat() method.
+    }
+
+    public function fly()
+    {
+        // TODO: Implement fly() method.
+    }
+}
---
+++ homework/hw_17/interfaces/BirdInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace homework\hw_17\interfaces;
+
+
+interface BirdInterface
+{
+    public function eat();
+}
